0b33fca9e8d9e11e968535eeb562270764da5c39
Sighmox <Sighmox@github.com>
2019-03-19 18:02:40 -0500

    First Commit of Programmer Resources

---
+++ Programmer Resources/DetailViewController.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.swift
+//  Programmer Resources
+//
+//  Created by Paul Baker on 3/19/19.
+//  Copyright © 2019 Paul Baker. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+}
---
+++ Programmer Resources/ResourceDataController.swift
@@ -0,0 +1,9 @@
+//
+//  ResourceDataController.swift
+//  Programmer Resources
+//
+//  Created by Paul Baker on 3/19/19.
+//  Copyright © 2019 Paul Baker. All rights reserved.
+//
+
+import Foundation
