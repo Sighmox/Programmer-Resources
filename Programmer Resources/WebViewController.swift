e167b581bcb3861a90d15e3ef65a4f232a2d498a
Sighmox <Sighmox@github.com>
2019-03-19 19:17:21 -0500

    WebView connection Completed

--- Programmer Resources/WebViewController.swift
+++ Programmer Resources/WebViewController.swift
@@ -6,8 +6,17 @@
 //  Copyright © 2019 Paul Baker. All rights reserved.
 //
 
+import WebKit
 import UIKit
 
 class WebViewController: UIViewController {
     
+    var url: URL!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let request = URLRequest(url: url)
+        webView.load(request)
+    }
 }
